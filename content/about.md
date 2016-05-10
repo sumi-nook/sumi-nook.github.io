43dd03c2836bc8b571f96ae52939f56bcef0bf04
sumi.nook <sumi.nook@live.jp>
2016-05-10 16:28:35 +0900

    Initial commit.

---
+++ content/about.md
@@ -0,0 +1,10 @@
+Title: About this site
+Date: 2100-01-01
+Category: Index
+
+# Nook
+
+## 作業中
+
+* Hestia: <https://sumi-nook.github.io/hestia/>
+
---
+++ pelicanconf.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'sumi-nook'
+SITENAME = u'Nook'
+SITEURL = ''
+
+TIMEZONE = 'Asia/Tokyo'
+
+DEFAULT_LANG = u'ja'
+
+THEME = './pelican-sober'
+GOOGLE_ANALYTICS = 'UA-77451117-1'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS =  (('Hestia', '/hestia/'),
+          ('Old site', 'https://sites.google.com/site/sumi2nook/'),)
+
+# Social widget
+SOCIAL = (('Twitter', 'https://twitter.com/S2_SUMI'),
+          ('GitHub', 'https://github.com/sumi-nook'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
